b4550b99e1ade618fbd13a52088f6b255b7a3033
Sam Hastings <samhastingsis@gmail.com>
2024-08-08 13:46:03 -0400

    Initial Commit

---
+++ MotaNet/MotaNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotaNetApp.swift
+//  MotaNet
+//
+//  Created by sam hastings on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MotaNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
