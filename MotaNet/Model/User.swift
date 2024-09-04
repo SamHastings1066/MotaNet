e937c7ee73b49ece51f6ba8589dacd8d17386001
Sam Hastings <samhastingsis@gmail.com>
2024-09-04 09:18:55 -0400

    Update Mock users to use firebase image url for progileImageUrl.

--- MotaNet/Model/User.swift
+++ MotaNet/Model/User.swift
@@ -25,10 +25,10 @@ struct User: Identifiable, Hashable, Codable {
 
 extension User {
     static var MOCK_USERS: [User] = [
-        .init(id: UUID().uuidString, username: "Black Panther", profileImageUrl: "https://firebasestorage.googleapis.com:443/v0/b/motanet-ca3e7.appspot.com/o/profile_images%2F9A43C364-8CCB-4A9C-913D-2CC1836C40F9?alt=media&token=ef074fb1-33b4-49ad-bb6c-62d758b8ea81", fullname: "Chadwick Bozeman", bio: "Black panther guy", email: "bp@gmail.com"),
-        .init(id: UUID().uuidString, username: "Venom", profileImageUrl: nil, fullname: "Eddie Brock", bio: "Venom", email: "venom@gmail.com"),
-        .init(id: UUID().uuidString, username: "Iron Man", profileImageUrl: nil, fullname: "One Bredda", bio: "ironman guy", email: "ironman@gmail.com"),
-        .init(id: UUID().uuidString, username: "Batman", profileImageUrl: nil, fullname: "Two Bredda", bio: "batman guy", email: "batman@gmail.com"),
-        .init(id: UUID().uuidString, username: "Spiderman", profileImageUrl: nil, fullname: "Tree Bredda", bio: "spiderman guy", email: "spiderman@gmail.com")
+        .init(id: "black_panther", username: "Black Panther", profileImageUrl: "https://firebasestorage.googleapis.com:443/v0/b/motanet-ca3e7.appspot.com/o/profile_images%2F9A43C364-8CCB-4A9C-913D-2CC1836C40F9?alt=media&token=ef074fb1-33b4-49ad-bb6c-62d758b8ea81", fullname: "Chadwick Bozeman", bio: "Black panther guy", email: "bp@gmail.com"),
+        .init(id: "venom", username: "Venom", profileImageUrl: nil, fullname: "Eddie Brock", bio: "Venom", email: "venom@gmail.com"),
+        .init(id: "iron_man", username: "Iron Man", profileImageUrl: nil, fullname: "One Bredda", bio: "ironman guy", email: "ironman@gmail.com"),
+        .init(id: "batman", username: "Batman", profileImageUrl: nil, fullname: "Two Bredda", bio: "batman guy", email: "batman@gmail.com"),
+        .init(id: "spiderman", username: "Spiderman", profileImageUrl: nil, fullname: "Tree Bredda", bio: "spiderman guy", email: "spiderman@gmail.com")
     ]
 }
